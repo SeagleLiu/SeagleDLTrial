7319df5b7aec20985943873fe3f53d980979e27e
SeagleLiu <seaglesoar@gmail.com>
2016-12-12 13:37:15 -0800

    My first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Seagle's trial on ML/DL/Spark, etc.
+Begin to import tensor flow
---
+++ Trial1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 12 13:22:18 2016
+
+@author: Seagle
+"""
+print('Hello World')
